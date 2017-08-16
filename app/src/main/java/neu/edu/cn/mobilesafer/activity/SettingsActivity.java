1a63414ea984cfe964f1ea09d4bb7bcaf7c8b580
henryneu <neuyimi@gmail.com>
2017-08-16 22:22:12 +0800

    设置中心相关代码逻辑

--- app/src/main/java/neu/edu/cn/mobilesafer/activity/SettingsActivity.java
+++ app/src/main/java/neu/edu/cn/mobilesafer/activity/SettingsActivity.java
@@ -1,15 +1,35 @@
 package neu.edu.cn.mobilesafer.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import neu.edu.cn.mobilesafer.R;
+import neu.edu.cn.mobilesafer.ui.SettingsItem;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private SettingsItem mSettingsItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        // 初始化视图
+        initView();
+    }
+
+    /**
+     * 初始化视图
+     */
+    private void initView() {
+        mSettingsItem = (SettingsItem) findViewById(R.id.settings_item_update);
+        mSettingsItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isCheck = mSettingsItem.IsCheck();
+                mSettingsItem.setCheck(!isCheck);
+            }
+        });
     }
 }
--- app/src/main/java/neu/edu/cn/mobilesafer/ui/SettingsItem.java
+++ app/src/main/java/neu/edu/cn/mobilesafer/ui/SettingsItem.java
@@ -3,7 +3,9 @@ package neu.edu.cn.mobilesafer.ui;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import neu.edu.cn.mobilesafer.R;
 
@@ -12,6 +14,13 @@ import neu.edu.cn.mobilesafer.R;
  */
 
 public class SettingsItem extends RelativeLayout {
+
+    private TextView mTextTitle;
+
+    private TextView mTextDes;
+
+    private CheckBox mCheckBox;
+
     public SettingsItem(Context context) {
         this(context, null);
     }
@@ -23,5 +32,39 @@ public class SettingsItem extends RelativeLayout {
     public SettingsItem(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         View.inflate(context, R.layout.settings_item, this);
+        // 初始化View
+        initView();
+    }
+
+    /**
+     * 检查CheckBox的状态是否是选中状态，选中则开启，否则关闭
+     * @return
+     */
+    public boolean IsCheck() {
+        return mCheckBox.isChecked();
+    }
+
+    /**
+     * 如果传递的是true，则设置CheckBox为选中，否则为没选中，同时改变文本描述
+     * @param isCheck 是否作为开启的变量，由点击过程中传递
+     */
+    public void setCheck(boolean isCheck) {
+        mCheckBox.setChecked(isCheck);
+        if (isCheck) {
+            // 开启
+            mTextDes.setText("自动跟新已开启");
+        } else {
+            // 关闭
+            mTextDes.setText("自动跟新已关闭");
+        }
+    }
+
+    /**
+     * 初始化View
+     */
+    private void initView() {
+        mTextTitle = (TextView) findViewById(R.id.text_des);
+        mTextDes = (TextView) findViewById(R.id.text_des);
+        mCheckBox = (CheckBox) findViewById(R.id.check_box);
     }
 }
